b61082d3d6474d2f4c3d45b0a7e6ae9276364247
kalineh <kalin.eh@gmail.com>
2015-11-14 13:06:00 -0800

    add network test scenes

---
+++ Unity/Assets/00_Game/Scripts/EditorImportAdditive.cs
@@ -0,0 +1,27 @@
+﻿
+#if UNITY_EDITOR
+
+using UnityEditor;
+using UnityEngine;
+
+public class MenuTest : MonoBehaviour {
+
+	[MenuItem ("File/Load Scene [Additive]")]
+	static void Apply()
+    {
+		Debug.Log ("Doing Something...");
+
+        var path = AssetDatabase.GetAssetOrScenePath(Selection.activeObject);
+        if (path == null)
+        {
+            EditorUtility.DisplayDialog("Select Scene", "Select a scene", "Ok");
+            return;
+        }
+
+        Debug.LogFormat("Opening scene {0}...", path);
+
+        EditorApplication.OpenSceneAdditive(path);
+	}
+}
+
+#endif
---
+++ Unity/Assets/00_Game/Scripts/SimpleMover.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+
+public class SimpleMover
+    : MonoBehaviour
+{
+	private void Update()
+	{
+		if (Input.GetKeyDown(KeyCode.UpArrow))
+			transform.position += Vector3.up;
+
+		if (Input.GetKeyDown(KeyCode.DownArrow))
+			transform.position += Vector3.down;
+
+		if (Input.GetKeyDown(KeyCode.RightArrow))
+			transform.Rotate(Vector3.up, 45.0f);
+
+		if (Input.GetKeyDown(KeyCode.LeftArrow))
+			transform.Rotate(Vector3.right, 45.0f);
+	}
+}

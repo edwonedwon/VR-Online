0c9d894c0f5b8daffaf2516cd15f07e9755908f1
kalineh <kalin.eh@gmail.com>
2015-11-14 16:41:04 -0800

    add debug test scenes

---
+++ Unity/Assets/NetworkDebugTest.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using BeardedManStudios.Network;
+
+public class NetworkDebugTest
+	: MonoBehaviour
+{
+    public string IP = "108.47.46.240";
+    public int Port = 15937;
+    public bool Host = false;
+    public bool NAT = false;
+
+    public void Start()
+    {
+        if (Host)
+        {
+            Debug.Log("Hosting...");
+
+            NetWorker nw = Networking.Host((ushort)Port, Networking.TransportationProtocolType.UDP, 8, false, null, false, true, NAT);
+
+            nw.connected += () => Debug.Log("connected");
+            nw.disconnected += () => Debug.Log("disconnected");
+        }
+        else
+        {
+            Debug.Log("Client...");
+
+            NetWorker nw = Networking.Connect(IP, (ushort)Port, Networking.TransportationProtocolType.UDP, false, NAT);
+
+            nw.connected += () => Debug.Log("connected");
+            nw.disconnected += () => Debug.Log("disconnected");
+        }
+    }
+}
